7a4698e78e3efa01dc6483667c43a19db6e49a6b
Vikram Singh <singhvikram143777@gmail.com>
2021-03-28 18:17:38 +0530

    Hacker news

--- src/App.js
+++ src/App.js
@@ -1,71 +1,14 @@
-import React, { useState, useEffect } from "react";
-import { useFetch } from "./useFetch";
-import Follower from "./Follower";
+import React from "react";
+import SearchForm from "./SearchForm";
+import Stories from "./Stories";
+import Buttons from "./Buttons";
 function App() {
-  const { loading, data } = useFetch();
-  const [page, setPage] = useState(0);
-  const [followers, setFollowes] = useState([]);
-
-  useEffect(() => {
-    if (loading) return;
-    setFollowes(data[page]);
-  }, [loading, page]);
-
-  const nextPage = () => {
-    setPage((oldPage) => {
-      let nextPage = oldPage + 1;
-      if (nextPage > data.length - 1) {
-        nextPage = 0;
-      }
-      return nextPage;
-    });
-  };
-
-  const prevPage = () => {
-    setPage((oldPage) => {
-      let prevPage = oldPage - 1;
-      if (prevPage < 0) {
-        prevPage = data.length - 1;
-      }
-      return prevPage;
-    });
-  };
-
   return (
-    <main>
-      <div className="section-title">
-        <h1>{loading ? "loading" : "pagination"}</h1>
-        <div className="underline"></div>
-      </div>
-      <div className="followers">
-        <div className="container">
-          {followers.map((follower) => {
-            return <Follower key={follower.id} {...follower} />;
-          })}
-        </div>
-        {!loading && (
-          <div className="btn-container">
-            <button className="prev-btn" onClick={prevPage}>
-              prev
-            </button>
-            {data.map((item, index) => {
-              return (
-                <button
-                  key={index}
-                  className={`page-btn ${index === page ? "active-btn" : null}`}
-                  onClick={() => setPage(index)}
-                >
-                  {index + 1}
-                </button>
-              );
-            })}
-            <button className="next-btn" onClick={nextPage}>
-              next
-            </button>
-          </div>
-        )}
-      </div>
-    </main>
+    <>
+      <SearchForm />
+      <Buttons />
+      <Stories />
+    </>
   );
 }
 
